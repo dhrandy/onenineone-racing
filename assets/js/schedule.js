001878e2302b82d36765fb1e4e7b085c085ae781
dhrandy <dhrandy@gmail.com>
2018-09-03 12:55:29 -0400

    updated pics

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -26,3 +26,8 @@ $.getJSON(
       
     }
 );
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+};
