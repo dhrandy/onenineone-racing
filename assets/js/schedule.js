67207c8c24f9d0d86c75c7c0a023187e51825b05
dhrandy <dhrandy@gmail.com>
2018-09-03 14:39:31 -0400

    update

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -26,8 +26,3 @@ $.getJSON(
       
     }
 );
-
-window.onscroll = () => {
-  const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
-};
